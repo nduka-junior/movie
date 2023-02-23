6df5cba8abd987e29553ee9351b22cd814c0c961
nduka-junior <ndukajunior4@gmail.com>
2023-02-23 14:22:23 +0100

    added loading page

--- src/component/Trending.js
+++ src/component/Trending.js
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { fetchapi } from "../fetchapi";
 import TrendingList from "./TrendingList";
+import Loading from "./Loading";
 import { Pagination } from "@mui/material";
 import usePagination from "../usePagination";
 import "../styles/trending.css";
 
 function Trending() {
   const [trendings, settrendings] = useState([]);
-  const [totalResults,settotalResults] = useState()
-  
-   const [Page, setPage] = useState(1);
+  const [totalResults, settotalResults] = useState();
+  const [loading, setloading] = useState(false);
+
+  const [Page, setPage] = useState(1);
   const apifetch = async () => {
+    setloading(true);
     const data = await fetchapi(
-      "https://api.themoviedb.org/3/trending/all/week?",Page
+      "https://api.themoviedb.org/3/trending/all/week?",
+      Page
     );
+
     settrendings(data.results);
     settotalResults(data.total_results);
+    setloading(false);
   };
   const _DATA = usePagination(trendings, 20);
-  
-    const pag = (e, value) => {
-      setPage(value);
-      _DATA.jump(value);
-      console.log(_DATA );
-      apifetch();
-    };
+
+  const pag = (e, value) => {
+    setPage(value);
+    _DATA.jump(value);
+    console.log(_DATA);
+    apifetch();
+  };
   useEffect(() => {
     apifetch();
   }, []);
@@ -33,12 +39,23 @@ function Trending() {
     <>
       <h1>Trending</h1>
 
-      <div className='cc'>
-        <div className="single_trend">
-          {trendings.map((trending, index) => {
-            return <TrendingList trending={trending} key={index} />;
-          })}
-        </div>
+      <div className="cc">
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="single_trend">
+            {trendings.map((trending, index) => {
+              return (
+                <TrendingList
+                  trending={trending}
+                  key={index}
+                  loading={loading}
+                />
+              );
+            })}
+          </div>
+        )}
+
         <Pagination
           sx={{ mt: 10, mb: 10 }}
           count={Math.ceil(totalResults / 20)}
