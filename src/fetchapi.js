f6cad29ce7af395487c7f8bc476da7d054fac0f0
nduka-junior <ndukajunior4@gmail.com>
2023-02-23 20:49:16 +0100

    update api key

--- src/fetchapi.js
+++ src/fetchapi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-export const fetchapi = (url,page=1) => {
-  const urlconcat = `${url}api_key=8a2208e5a0ce32e0e5044f64bc78c38d&page=${page}`;
-  console.log(urlconcat)
+export const fetchapi = (url, page = 1) => {
+  const urlconcat = `${url}api_key=${process.env.REACT_APP_API_KEY}&page=${page}`;
+  console.log(urlconcat);
   return axios.get(urlconcat).then(({ data }) => {
-   return data
+    return data;
   });
 };
