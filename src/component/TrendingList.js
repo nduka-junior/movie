53fbb72b60cb69d78be1a3fefdc0cae2a5e63dd8
nduka-junior <ndukajunior4@gmail.com>
2023-02-23 21:10:14 +0100

    added ratings

--- src/component/TrendingList.js
+++ src/component/TrendingList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../styles/trending.css";
 import { Link } from "react-router-dom";
-
+import Rating from "@mui/material/Rating";
 function TrendingList({ trending }) {
+  console.log(trending)
   return (
     <>
       {trending.title && (
@@ -15,6 +16,23 @@ function TrendingList({ trending }) {
           <div className="trendlist_text">
             <h4>Title: {trending.title}</h4>
             <Link to={`/movies/${trending.id}`}> Overview</Link>
+          </div>
+          <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop:"10px"
+
+        
+          }}>
+        
+            <Rating
+              name="read-only"
+              value={trending.vote_average / 2}
+              precision={0.01}
+              readOnly
+            />{" "}
+            <h4>{trending.vote_average}</h4>
           </div>
         </div>
       )}
